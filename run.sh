15fb50e5513b76d3267b67261e76d7ae03ac85d0
Yann Chemin <ychemin@gmail.com>
2016-02-24 11:54:35 +0100

    Added convert2proj4 bash script in run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ rm -f outputIAU200*.csv
 ./iau2wkt naifcodes_radii_m_wAsteroids_IAU2000.csv outputIAU2000.csv
 ./iau2wkt naifcodes_radii_m_wAsteroids_IAU2009.csv outputIAU2009.csv
 
+
+#for line in $(tail -n +2 outputIAU2000.csv | cut -d ',' -f2-); do gdalsrsinfo $line -o proj4 ; done
+
